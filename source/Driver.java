baa5661b8ac9f3c454721feefe34898ea0cc85d4
Tiger <tgsachse@gmail.com>
2018-09-03 20:20:56 -0400

    working on database api

--- source/Driver.java
+++ source/Driver.java
@@ -1,11 +1,11 @@
 package escala;
 
 import java.sql.*;
-import escala.database.Portal;
+import escala.database.*;
 
 public class Driver {
     public static void main(String[] args) throws SQLException {
         Portal portal = new Portal();
-        portal.printMe();
+        Region region1 = portal.getRegion("USA");
     }
 }
--- source/database/Portal.java
+++ source/database/Portal.java
@@ -4,10 +4,81 @@ import java.sql.*;
 import org.apache.derby.jdbc.*;
 
 public class Portal {
-    public void printMe() throws SQLException {
-        Driver driver = new EmbeddedDriver();
-        DriverManager.registerDriver(driver);
-        Connection connection = DriverManager.getConnection("jdbc:derby:data/regions;create=true");
-    }    
 
+    private Driver driver;
+    private Connection connection;
+
+    public Portal() throws SQLException {
+        driver = new EmbeddedDriver();
+        DriverManager.registerDriver(driver);
+        connection = DriverManager.getConnection("jdbc:derby:data/tables;create=true");
+
+        String[] tables = {"regions", "events"};
+
+        for (String table : tables) {
+            DatabaseMetaData data = connection.getMetaData();
+            ResultSet results = data.getTables(null, null, table.toUpperCase(), null);
+            if (!results.next()) {
+                System.out.println("creating table" + table);
+                createTable(table);
+            }
+        }
+    }
+
+    private void createTable(String name) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        if (name.equals("regions")) {
+            statement.executeUpdate(
+                "CREATE TABLE regions (" + 
+                "name VARCHAR(15) PRIMARY KEY, " +
+                "entryCost INT, efficiencyCost INT, " +
+                "logisticsCost INT, " +
+                "marketingCost INT, " +
+                "taxRate INT)"
+            );
+        }
+        else if (name.equals("events")) {
+            statement.executeUpdate(
+                "CREATE TABLE events (" +
+                "name VARCHAR(15) PRIMARY KEY, " +
+                "heading VARCHAR(15), description VARCHAR(300), " +
+                "logisticsChange INT, marketingChange INT, " +
+                "productChange INT, cashChange INT)"
+            );
+        }
+
+        if (statement != null) {
+            statement.close();
+        }
+    }
+    
+    public Region getRegion(String name) throws SQLException {
+        Statement statement = connection.createStatement();
+        String search = "SELECT * FROM regions WHERE name='" + name + "'";
+        ResultSet results = statement.executeQuery(search);
+
+        if (results.next() == false) {
+            return null;
+        }
+
+        Region region = new Region(results.getString("name"),
+                                   results.getInt("entryCost"),
+                                   results.getInt("efficiencyCost"),
+                                   results.getInt("logisticsCost"),
+                                   results.getInt("marketingCost"),
+                                   results.getInt("taxRate"));
+
+        if (statement != null) {
+            statement.close();
+        }
+
+        return region;
+    }
+
+    public void close() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        } 
+    }
 }

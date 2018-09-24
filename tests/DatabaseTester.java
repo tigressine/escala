45e294fc470cd2840abb1233c8d8143d46f56c18
Tiger <tgsachse@gmail.com>
2018-09-24 12:12:03 -0400

    initial unrefactored test operation function

--- tests/DatabaseTester.java
+++ tests/DatabaseTester.java
@@ -1,7 +1,12 @@
+package tests;
+
+import escala.*;
+import java.sql.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class DatabaseTester {
+    @Test
     public void testPortalCanOpen() {
         try {
             Portal portal = new Portal();

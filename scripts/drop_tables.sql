83212191efaaeece8280cc9455c0f9d16be48a18
Tiger <tgsachse@gmail.com>
2018-10-03 13:31:22 -0400

    initial new tables

--- scripts/drop_tables.sql
+++ scripts/drop_tables.sql
@@ -6,5 +6,7 @@ Written by Tiger Sachse.
 CONNECT 'jdbc:derby:../data/tables';
 DROP TABLE regions;
 DROP TABLE events;
+DROP TABLE skill_edges;
+DROP TABLE skill_nodes;
 DISCONNECT;
 EXIT;
--- scripts/make_tables.sql
+++ scripts/make_tables.sql
@@ -4,18 +4,40 @@ Written by Tiger Sachse.
 */
 
 CONNECT 'jdbc:derby:../data/tables;create=true';
-CREATE TABLE regions (name VARCHAR(15) PRIMARY KEY,
-                      taxRate REAL,
-                      entryCost REAL,
-                      logisticsCost INT,
-                      marketingCost INT,
-                      efficiencyCost INT);
-CREATE TABLE events (name VARCHAR(15) PRIMARY KEY,
-                     description VARCHAR(300),
-                     alignment REAL,
-                     cashEffect REAL,
-                     logisticsEffect INT,
-                     marketingEffect INT,
-                     efficiencyEffect INT);
+CREATE TABLE regions (
+    name VARCHAR(15) PRIMARY KEY,
+    taxRate REAL,
+    entryCost REAL,
+    logisticsCost INT,
+    marketingCost INT,
+    efficiencyCost INT
+);
+CREATE TABLE events (
+    name VARCHAR(15) PRIMARY KEY,
+    description VARCHAR(300),
+    alignment REAL,
+    cashEffect REAL,
+    logisticsEffect INT,
+    marketingEffect INT,
+    efficiencyEffect INT
+);
+CREATE TABLE skill_nodes (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    description VARCHAR(300),
+    cost REAL,
+    logisticsEffect INT,
+    marketingEffect INT,
+    efficiencyEffect INT
+);
+CREATE TABLE skill_edges (
+    start_node INT,
+    follow_node INT,
+    PRIMARY KEY (start_node, follow_node),
+    FOREIGN KEY (start_node)
+    REFERENCES skill_nodes,
+    FOREIGN KEY (follow_node)
+    REFERENCES skill_nodes
+);
 DISCONNECT;
 EXIT;

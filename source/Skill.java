bd253b89e8586d5d3ce914f8ad38860042db12e0
Tiger Sachse <tiger@sachse.io>
2018-10-04 23:29:01 -0400

    skill tree semi-functional

--- source/Skill.java
+++ source/Skill.java
@@ -1,6 +1,9 @@
+// Part of Escala.
+// Written by Tiger Sachse.
 
 package escala;
 
+// Holds information about a skill in a skill tree.
 public class Skill {
     private int id;
     private String name;
@@ -10,6 +13,7 @@ public class Skill {
     private int marketingEffect;
     private int efficiencyEffect;
 
+    // Construct a new skill.
     public Skill(
         int id,
         String name,
@@ -28,7 +32,23 @@ public class Skill {
         this.efficiencyEffect = efficiencyEffect;
     }
 
+    // Get the ID of this skill.
     public int getID() {
         return id;
+    }
+
+    // Return a string holding this skill's information.
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+    
+        builder.append(String.format("id: %d\n", id));
+        builder.append(String.format("name: %s\n", name));
+        builder.append(String.format("description: %s\n", description));
+        builder.append(String.format("cost: %f\n", cost));
+        builder.append(String.format("logistics: %d\n", logisticsEffect));
+        builder.append(String.format("marketing: %d\n", marketingEffect));
+        builder.append(String.format("efficiency: %d", efficiencyEffect));
+
+        return builder.toString();
     }
 }
--- source/SkillTree.java
+++ source/SkillTree.java
@@ -6,33 +6,43 @@ package escala;
 import escala.*;
 import java.util.*;
 
+// Holds a graph of skills.
 public class SkillTree {
     private String treeID;
     private HashMap<Integer, Node> nodes;
 
+    // Construct a new skill tree with an ID.
     public SkillTree(String treeID) {
         this.treeID = treeID;
         nodes = new HashMap<>();
     }
 
+    // Add a skill and its children to the tree.
     public void addSkill(Skill skill, HashSet<Integer> children) {
         nodes.put(skill.getID(), new Node(skill, children));
     }
 
-    public void print() {
+    // Return a string representation of the tree.
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
         for (Map.Entry<Integer, Node> entry : nodes.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
+            builder.append(entry.getValue());
+            builder.append("\n");
         }
+
+        return builder.toString();
     }
 }
 
+// Holds a skill and graph information in a skill tree.
 class Node {
     private Skill skill;
     private int parents;
     private boolean purchased;
     private HashSet<Integer> children;
 
+    // Create a new node with a skill and a specified child set.
     public Node(Skill skill, HashSet<Integer> children) {
         this.skill = skill;
         this.children = children;
@@ -41,7 +51,24 @@ class Node {
         purchased = false;
     }
 
+    // Add to the parent count for this node.
     public void incrementParents() {
         parents++;
+    }
+
+    // Create a string representation of this node.
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(skill.toString() + "\n");
+        builder.append(String.format("purchased: %s\n", String.valueOf(purchased)));
+        builder.append(String.format("parents: %d\n", parents));
+        builder.append("children:");
+        for (Integer child : children) {
+            builder.append(String.format(" %s", child.toString()));
+        }
+        builder.append("\n");
+
+        return builder.toString();
     }
 }
